fd971c16d9a9b87628a44dd1be48e6af197dd505
StyvensB <s.belloge@gmail.com>
2015-05-18 18:16:27 -0400

    add accumulate filter for OCR

--- titleOCR.py
+++ titleOCR.py
@@ -3,17 +3,31 @@
 import Image
 import pytesseract
 import cv2
+import numpy
 import sys
 import os
 import argparse
 
 parser = argparse.ArgumentParser(description='Recover the title of the chapters from the main video')
 parser.add_argument('path',help="path of the video file")
-parser.add_argument('-s','--substract',type=int,dest="background",help="rank of the frame to substract")
 parser.add_argument('-d','--delay',type=int,dest="delay",default=0,help="frame number to skip after chapter start")
-parser.add_argument('-c','--color',dest="color",nargs=3,type=int,help="specify color filtering [0..255] (R G B)")
-parser.add_argument('-r','--radius',dest="colorTolerance",default=30,type=int,help="color tolerance (radius) (30)")
+parser.add_argument('-m','--method',dest="method",choices=['subtract','color','accumulate'],help="Method to use to highlight the title")
+# Method Specific Options
+#	Background subtraction
+g1=parser.add_argument_group('Substract')
+g1.add_argument('--background',type=int,dest="background",help="rank of the frame to subtract")
+#	Colour Filtering
+g2=parser.add_argument_group('Color')
+g2.add_argument('--color',dest="color",nargs=3,type=int,help="specify color used by filter [0..255] (R G B)")
+g2.add_argument('--tolerance',dest="colorTolerance",default=30,type=int,help="color tolerance (radius) (30)")
+#	Time Accumulation
+g3=parser.add_argument_group('Accumulate')
+g3.add_argument('--samples',dest="sample",default=10,type=int,help="number of samples to accumulate(10)")
+g3.add_argument('--span',dest="span",default=50,type=int,help="number of frames on which to accumulate (50)")
+
 args=parser.parse_args()
+
+# Check Argument validity
 if not os.path.exists(args.path):
 	print "Can't find file "+ args.path
 	sys.exit(1)
@@ -24,18 +38,25 @@ if not cap.isOpened():
 	sys.exit(1)
 
 delay=args.delay
-if args.background is not None :
-	cap.set(1,args.background)
-	ret,imgRef = cap.read()
-else :
-	imgRef =None
+width  = cap.get(3) # CV_CAP_PROP_FRAME_WIDTH
+height = cap.get(4) # CV_CAP_PROP_FRAME_HEIGHT
 
-if args.color is not None :
-	colRef	= (args.color[2],args.color[1],args.color[0])
-	colThres= args.colorTolerance
-else :
-	colRef = None
+if args.method == 'substract':
+	if args.background is not None :
+		cap.set(1,args.background)
+		ret,imgRef = cap.read()
+	else:
+		print "substract method need BACKGROUND"
+		sys.exit(1)
+elif args.method == 'color':
+	if args.color is not None :
+		colRef	= (args.color[2],args.color[1],args.color[0])
+		colThres= args.colorTolerance
+	else :
+		print "color method need COLOR"
+		sys.exit(1)
 
+# Open the necessary Files
 (baseP,extP)=os.path.splitext(args.path)
 titleImgOutP=baseP+"_title"
 if not os.path.exists(titleImgOutP):
@@ -43,25 +64,33 @@ if not os.path.exists(titleImgOutP):
 frameIn=open(baseP+"_frame.csv",'r')
 titleOut=open(baseP+"_title.csv",'w')
 
-
+#Start Processing Loop
 i=1
 for line in frameIn: 
 	frNB=int(line)
 	cap.set(1,frNB+delay)
 	ret,imgTitle = cap.read()
 	cv2.imwrite(titleImgOutP+"\\%02d_org.jpg" % i,imgTitle)
-	if imgRef is not None:
+	if args.method == 'substract':
 		imgCV=cv2.absdiff(imgTitle,imgRef)
 		imgCV = cv2.cvtColor(imgCV,cv2.COLOR_BGR2GRAY)
 		ret,imgCV=cv2.threshold(imgCV,30,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 		#print ret
-	elif colRef is not None:
-		imgCV=cv2.cvtColor(imgTitle,cv2.COLOR_BGR2GRAY)
+	elif args.method == 'color':
+		imgCV=numpy.zeros((height,width), numpy.uint8)
+		imgCV[:]=255
 		for j in range(0,3):
 			ret,chal1=cv2.threshold(imgTitle[:,:,j],colRef[j]-colThres,255,cv2.THRESH_BINARY)
 			ret,chal2=cv2.threshold(imgTitle[:,:,j],colRef[j]+colThres,255,cv2.THRESH_BINARY_INV)
-			imgFilter=cv2.multiply(chal1,chal2)
-			imgCV=cv2.multiply(imgCV,imgFilter)
+			imgFilter=cv2.bitwise_and(chal1,chal2)
+			imgCV=cv2.bitwise_and(imgCV,imgFilter)
+	elif args.method == 'accumulate':
+		imgCV=cv2.cvtColor(imgTitle,cv2.COLOR_BGR2GRAY)
+		for j in range(1,args.sample):
+			cap.set(1,frNB+delay+j*int(args.span/args.sample))
+			ret,imgTemp = cap.read()
+			imgTemp=cv2.cvtColor(imgTemp,cv2.COLOR_BGR2GRAY)
+			imgCV=cv2.bitwise_and(imgCV,imgTemp)
 	else:
 		imgCV=imgTitle
 		imgCV = cv2.cvtColor(imgCV,cv2.COLOR_BGR2RGB)
@@ -69,6 +98,7 @@ for line in frameIn:
 	pil_im = Image.fromarray(imgCV)
 	#pil_im.show()
 	pil_im.save(titleImgOutP+"\\%02d.jpg" % i)
+	#Perform the OCR
 	title = pytesseract.image_to_string(pil_im,lang='eng')
 	title=title.replace('\n',' ').replace('\r','')
 	print i, title
