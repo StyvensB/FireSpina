5a9ad2eb3801b50784b169b9e99620cb6a5b1f6c
StyvensB <s.belloge@gmail.com>
2015-05-18 18:27:22 -0400

    made rename.py reversible and more robust

--- rename.py
+++ rename.py
@@ -1,12 +1,14 @@
-# python "C:\Users\Styvens\Documents\dvd_remix\titleOCR.py" C:\Users\Styvens\Documents\Favela Jiu Jitsu\Disc 1 Open Guard Passes.avi" 50 2
 
 import glob
 import sys
 import os
+import string
+
 
 (baseP,extP)=os.path.splitext(sys.argv[1])
 baseN=os.path.basename(baseP)
 print baseP+"_title.csv",baseP+"_chapter"
+
 if not os.path.exists(baseP+"_chapter"):
 	print "chapters not found"
 	sys.exit(1)
@@ -19,14 +21,19 @@ titleIn=open(baseP+"_title.csv",'r')
 
 for line in titleIn: 
 	index,title = line.rstrip('\n').split(',',1)
+	title=title.translate(string.maketrans("\/?*%","__---"))
+	
 	res=glob.glob(os.path.join(baseP+"_chapter",index+"*") )
 	if not res:
 		continue
 	src=res[0]
 	(dstB,dstX)=os.path.splitext(src)
-	dst =dstB+"_"+title+dstX 
+	dst =os.path.join(baseP+"_chapter",index)+"_"+title+dstX 
 	print src,"->",dst
-	os.rename(src,dst)
+	try:
+		os.rename(src,dst)
+	except WindowsError as detail:
+		print detail
 
 titleIn.close()
 sys.exit()
